ac20b610b6cc3b640e80bf921ba1ef6d802d56cc
JM <jmalmeida700@gmail.com>
2023-11-30 18:37:26 -0300

    configurar smtp

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teste php</title>
 </head>
 <body>
-<?php
-    echo "OLÁ GIT!";
-?>    
+    <div class="container">
+        <p><a href="login.php">Login</a></p>
+        <p><a href="dashboard.php">Dashboard</a></p>
+    </div>
 </body>
 </html>
